7af6ef2807ba4baca2683e1b701df49f956d4687
berrymot <merrybot47@gmail.com>
2024-10-04 13:15:55 -0700

    switch tangut font + add iosevkatoaq + move css

--- src/bin/noto.rs
+++ src/bin/noto.rs
@@ -1,7 +1,8 @@
 use std::fs;
 
 use itertools::Itertools;
-use notoize::NotoizeClient;
+use notoize::{Font, NotoizeClient};
+use reqwest::blocking::Client;
 
 fn main() {
     let json = fs::read_to_string("data/toakue.js").unwrap();
@@ -14,8 +15,24 @@ fn main() {
         .into_iter()
         .sorted_by_key(|x| x.fontname.contains("CJK"))
         .collect_vec();
-    fonts.retain(|f| !["Noto Color Emoji"].contains(&f.fontname.as_str()));
+    fonts.retain(|f| !["Noto Color Emoji", "Noto Serif Tangut"].contains(&f.fontname.as_str()));
+    let client = Client::new();
+    #[allow(clippy::single_element_loop)]
+    for (url, filename, fontname) in [(
+        "https://www.babelstone.co.uk/Fonts/Download/TangutYinchuan.ttf",
+        "TangutYinchuan.ttf",
+        "Tangut Yinchuan",
+    )] {
+        fonts.push(Font {
+            filename: filename.to_string(),
+            fontname: fontname.to_string(),
+            bytes: client.get(url).send().unwrap().bytes().unwrap().to_vec(),
+        });
+    }
     let mut css = String::new();
+    for font in fs::read_dir("fonts").unwrap() {
+        fs::remove_file(font.unwrap().path()).unwrap();
+    }
     for font in fonts.clone() {
         fs::write(format!("fonts/{}", font.filename), font.bytes).unwrap();
         css += &format!(
