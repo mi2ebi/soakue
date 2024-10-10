e2be645909af36f1b29387b9cc0872d9efd6c608
berrymot <merrybot47@gmail.com>
2024-10-09 21:49:24 -0700

    Revert "ꝡohào ka"
    This reverts commit fa422543f2daed36df9431ce5415bdcdf315aa9c.

---
+++ events.js
@@ -0,0 +1,50 @@
+var worker = {"postMessage": function(a) {}};
+var page, q = "", res = [];
+window.addEventListener("scroll", function(e) {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 100) {
+        page++;
+        load(res, page);
+        checkLength();
+    }
+});
+function checkLength() {
+    if (res && (page + 1) * 100 - 1 >= res.length) {
+        $`bottom`.innerHTML = res.length ? "ꝡofào ka" : "";
+    }
+}
+function clearRes() {
+    [`res`, `len`, `bottom`].forEach(x => $(x).innerHTML = "");
+}
+function redirect() {
+    var v = "?";
+    if (q) v += "&q=" + encodeURIComponent(q);
+    v = v.replace(/\?&/g, "?").replace(/[?&]+$/, "");
+    window.history.pushState(null, null, window.location.href.split("?")[0] + v);
+}
+var timer;
+$`search`.addEventListener("input", function() {
+    clearTimeout(timer);
+    q = $`search`.value.trim();
+    res = null;
+    clearRes();
+    // todo: handle `=` + tones/ıꝡ here rather than in `worker.js`
+    redirect();
+    $`bottom`.innerHTML = "chum lao jí pó jóaıse"
+    timer = setTimeout(function() {
+        if (q.length) {
+            worker.postMessage({"q": q})
+        } else {
+            res = null;
+            clearRes();
+            page = 0;
+        }
+    }, 100);
+});
+$`clear`.addEventListener("click", function() {
+    $`search`.value = "";
+    $`search`.focus();
+    dispatchSearch();
+});
+function dispatchSearch() {
+    $`search`.dispatchEvent(new Event("input", {"bubbles": true}));
+}
---
+++ src/bin/data.rs
@@ -0,0 +1,177 @@
+use chrono::Utc;
+use itertools::Itertools;
+use regex::bytes::{Regex, RegexBuilder};
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_str, to_string};
+use std::{fs, sync::LazyLock, time::Duration};
+use unicode_normalization::UnicodeNormalization;
+
+fn main() {
+    let client = Client::builder()
+        .timeout(Duration::from_secs(60))
+        .build()
+        .unwrap();
+    let query = r#"{"action": "search", "query": ["and"]}"#.to_string();
+    let res = client
+        .post("https://toadua.uakci.space/api")
+        .body(query.clone())
+        .send();
+    let text = res.unwrap().text().unwrap();
+    let dict = dictify(&text);
+    let dict_str = to_string(&dict).unwrap();
+    fs::write("data/toakue.js", format!("const dict = {dict_str};")).unwrap();
+    fs::write(
+        "data/all.txt",
+        dict.iter()
+            .map(|toa| toa.head.clone())
+            .collect_vec()
+            .join("\r\n"),
+    )
+    .unwrap();
+    fs::write("data/time.txt", format!("{:?}", Utc::now())).unwrap();
+}
+
+fn dictify(the: &str) -> Vec<Toa> {
+    from_str::<Toadua>(the)
+        .unwrap()
+        .results
+        .into_iter()
+        .filter(|toa| toa.score >= -2)
+        .map(|toa| {
+            let tone_info = tones(&toa.head);
+            (
+                (
+                    tone_info.0,
+                    tone_info.1,
+                    tone_info.2,
+                    -toa.score,
+                    toa.clone().scope,
+                    toa.clone().date,
+                    toa.clone().body,
+                ),
+                toa,
+            )
+        })
+        .sorted_by_key(|(info, _)| info.clone())
+        .map(|(_, toa)| toa)
+        .collect_vec()
+}
+
+static DOT_TONE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new("\u{0323}([\u{0301}\u{0302}\u{0308}])").unwrap());
+static PALATAL: LazyLock<Regex> = LazyLock::new(|| {
+    RegexBuilder::new("([ncs])h")
+        .case_insensitive(true)
+        .build()
+        .unwrap()
+});
+const TONES: &str = "\u{0300}\u{0301}\u{0308}\u{0302}";
+const _CONSONANTS: &str = "[bcdfghjklmnpqrstvz'ʰBCDFGHJKLMNPQRSTVZ]";
+const _VOWELS: &str = "[aeiouAEIOU]";
+// static CONSONANTS: LazyLock<Regex> = LazyLock::new(|| Regex::new(_CONSONANTS).unwrap());
+static VOWELS: LazyLock<Regex> = LazyLock::new(|| Regex::new(_VOWELS).unwrap());
+static FIND_STEM: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(&format!("\u{0323}({_CONSONANTS}+{_VOWELS})")).unwrap());
+fn tones(head: &str) -> (String, Vec<usize>, Vec<usize>) {
+    let head = String::from_utf8(
+        PALATAL
+            .replace(
+                &DOT_TONE.replace(
+                    head.nfd()
+                        .to_string()
+                        .to_lowercase()
+                        .trim_start_matches(|c| "*-@., ".contains(c))
+                        .chars()
+                        .map(|c| match c {
+                            'ı' => 'i',
+                            'ꝡ' => 'v',
+                            x => x,
+                        })
+                        .filter(|c| !"()".contains(*c))
+                        .collect::<String>()
+                        .as_bytes(),
+                    "$1\u{0323}".as_bytes(),
+                ),
+                "$1ʰ".as_bytes(),
+            )
+            .to_vec(),
+    )
+    .unwrap();
+    let mut tones = vec![];
+    let mut nat_indices = vec![];
+    let mut moved = vec![];
+    for word in head.split_whitespace() {
+        let mut tone = 1;
+        if !word.contains(|c| format!("\u{0323}{TONES}").contains(c)) {
+            moved.push(
+                String::from_utf8(
+                    VOWELS
+                        .replace(word.as_bytes(), "$0\u{0300}".as_bytes())
+                        .to_vec(),
+                )
+                .unwrap(),
+            );
+        }
+        for c in word.chars() {
+            if c == '\u{0323}' {
+                moved.push(
+                    String::from_utf8(
+                        FIND_STEM
+                            .replace(word.as_bytes(), format!("$1{}", n2t(tone)).as_bytes())
+                            .to_vec(),
+                    )
+                    .unwrap(),
+                );
+            }
+            if TONES.contains(c) {
+                tone = t2n(c);
+                moved.push(word.to_string());
+            }
+        }
+    }
+    let moved = moved.join(" ");
+    for (i, c) in moved.chars().enumerate() {
+        if TONES.contains(c) {
+            nat_indices.push(i - 1 - tones.len());
+            tones.push(t2n(c));
+        }
+    }
+    if moved.ends_with('-') {
+        *tones.iter_mut().last().unwrap() = 9;
+    }
+    let head = moved
+        .replace(|c| TONES.contains(c), "")
+        .trim_end_matches('-')
+        .to_string();
+    (head, tones, nat_indices)
+}
+
+fn t2n(c: char) -> usize {
+    TONES.chars().position(|t| t == c).unwrap() + 1
+}
+fn n2t(n: usize) -> char {
+    TONES.chars().nth(n - 1).unwrap()
+}
+
+#[derive(Deserialize, Serialize)]
+struct Toadua {
+    results: Vec<Toa>,
+}
+#[derive(Deserialize, Serialize, Clone)]
+struct Toa {
+    id: String,
+    date: String,
+    head: String,
+    body: String,
+    user: String,
+    notes: Vec<Note>,
+    score: i32,
+    scope: String,
+}
+#[derive(Deserialize, Serialize, Clone)]
+struct Note {
+    date: String,
+    user: String,
+    content: String,
+}
